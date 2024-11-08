8ef0cc27e31eb90e72b359bad22b56d88b9bafab
Eyiladeogo <adedayoeyiladeogo22@gmail.com>
2024-11-08 18:16:55 +0100

    Removed ending slashes from cors allowed origin urls

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -55,8 +55,8 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://bouncehq.vercel.app/",
-    "http://localhost:3000/"
+    "https://bouncehq.vercel.app",
+    "http://localhost:3000"
 ]
 
 MIDDLEWARE = [
